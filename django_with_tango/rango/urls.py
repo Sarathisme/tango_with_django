f56a568604e6c57ade4a61e875a689d528425661
Sarath Sattiraju <sarath.sattiraju@gmail.com>
2018-06-22 19:47:46 +0530

    Completed templates. Working on custom template tags

--- django_with_tango/rango/urls.py
+++ django_with_tango/rango/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, re_path, include
+from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
-    path('',views.index),
-    path('about/', views.about),
-    path('category/<categoryname>', views.show_category)
+    url(r'^$',views.index, name='index'),
+    url(r'^about/$', views.about, name="about"),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^category/(?P<category_name>[\w\-]+)/$', views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
 ]
--- django_with_tango/rango/views.py
+++ django_with_tango/rango/views.py
@@ -3,23 +3,21 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
-    context = RequestContext(request)
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {"categories": category_list}
-    return render_to_response('rango/index.html', context_dict, context)
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    context = RequestContext(request)
     data = {"linkname" : "Go back to Home page"}
-    return render_to_response("rango/about.html", data, context)
+    return render(request, "rango/about.html", data)
 
-def show_category(request, categoryname):
+def show_category(request, category_name):
     context_dict = {}
-    context = RequestContext(request)
     try:
-        category = Category.objects.get(slug=categoryname)
+        category = Category.objects.get(slug=category_name)
         pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
         context_dict['category'] = category
@@ -28,5 +26,46 @@ def show_category(request, categoryname):
         context_dict['category'] = None
         context_dict['pages'] = None
 
-    return render_to_response('rango/category.html', context_dict, context)
+    return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form':form})
+
+def add_page(request, category_name_slug):
+    form = PageForm()
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            page = form.save(commit=False)
+            page.category = category
+            page.views = 0
+            page.save()
+            return show_category(request, category_name_slug)
+        else:
+            print(form.errors)
+
+    context_dict = {'form':form, 'category':category}
+    return render(request, 'rango/add_page.html', context_dict)
+
+
+
+
     
